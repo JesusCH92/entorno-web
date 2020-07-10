91aae48853cc7a76aa5320b6e3f35ec2f489dfd7
JesusCH92 <jcocaharo@hotmail.com>
2020-07-10 03:13:00 +0200

    show php info in my server

--- src/index.php
+++ src/index.php
@@ -2,7 +2,9 @@
 
 echo 'works?' . PHP_EOL;
 
-$redis = new Redis();
-$redis->connect('redis');
+// $redis = new Redis();
+// $redis->connect('redis');
 
-echo $redis->exists($robot['name']) . PHP_EOL;  // ! si no da error estoy conectado a redis
+// echo $redis->exists($robot['name']) . PHP_EOL;  // ! si no da error estoy conectado a redis
+
+phpinfo();
